a4d08507714dbe3357194592f401247819b70758
wisskirchenj <juergen.wisskirchen@cofinpro.de>
2023-01-30 10:02:34 +0100

    Implement stage 1 and make_dist run config

---
+++ src/creditcalc/main.py
@@ -0,0 +1,13 @@
+LOAN_PRINCIPAL = 'Loan principal: 1000'
+FINAL_OUTPUT = 'The loan has been repaid!'
+FIRST_MONTH = 'Month 1: repaid 250'
+SECOND_MONTH = 'Month 2: repaid 250'
+THIRD_MONTH = 'Month 3: repaid 500'
+
+
+def main():
+    print(LOAN_PRINCIPAL, FIRST_MONTH, SECOND_MONTH, THIRD_MONTH, FINAL_OUTPUT, sep='\n')
+
+
+if __name__ == '__main__':
+    main()
